ddaa609da4495958e739e78ce06a32aa42ba7c8c
Douglas Vidal <vidaldl01@gmail.com>
2023-09-29 16:57:48 -0300

    add timestamps true weather model

--- src/models/Weather.ts
+++ src/models/Weather.ts
@@ -2,29 +2,33 @@ import { Decimal128 } from 'mongodb';
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const weatherSchema = new Schema({
-    temp: {
-        type: Decimal128,
-        required: true,
+const weatherSchema = new Schema(
+    {
+        temp: {
+            type: Decimal128,
+            required: true,
+        },
+        sens_term: {
+            type: Decimal128,
+            required: true,
+        },
+        umid: {
+            type: Decimal128,
+            required: true,
+        },
+        datetime: {
+            type: Date,
+            required: true,
+        },
+        cidade: {
+            type: String,
+            required: true,
+        },
     },
-    sens_term: {
-        type: Decimal128,
-        required: true,
-    },
-    umid: {
-        type: Decimal128,
-        required: true
-    },
-    datetime: {
-        type: Date,
-        required: true
-    },
-    cidade: {
-        type: String,
-        required: true
+    {
+        timestamps: true
     }
-});
-
+);
 
 const Weather = mongoose.model('Weather', weatherSchema);
 
